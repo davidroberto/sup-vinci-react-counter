2315e7516f84682b436ba262aec2592989b5ac60
David Robert <contact@david-robert.fr>
2024-06-05 14:07:36 +0200

    Link react router

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,12 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
-    <header>
-      <img src="/logo.jpeg" alt="logo" />
-      <h1>Bonjour Sup de vinci</h1>
+    <header className="header">
+      <img className="logo" src="https://www.freepnglogos.com/uploads/pokemon-symbol-logo-png-31.png" alt="logo" />
+      <h1>Pokedex</h1>
       <nav>
-        <ul>
-          <li>Accueil</li>
-          <li>Produits</li>
+        <ul className="menu-list">
+          <li>
+            <Link to="/">Accueil</Link>
+          </li>
+          <li>
+            <Link to="/pokemons">Pokemon</Link>
+          </li>
         </ul>
       </nav>
     </header>
